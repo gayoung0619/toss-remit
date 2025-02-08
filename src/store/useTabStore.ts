5e735174fae7d3f892bf375a0055431c7f3afb99
gayoung <did-619@naver.com>
2025-02-08 19:26:05 +0900

    feat: add tab store

--- src/store/useTabStore.ts
+++ src/store/useTabStore.ts
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 
-export const useTabStore = create((set) => ({
+type TabState = {
+  selectedTab: number;
+  setSelectedTab: (index: number) => void;
+};
+
+export const useTabStore = create<TabState>((set) => ({
   selectedTab: 0,
   setSelectedTab: (index) => set({ selectedTab: index })
 }));
