bf6650e1713ddf338ee76f2f22475b241dc1aab8
gayoung <did-619@naver.com>
2025-02-08 21:38:25 +0900

    feat: add all api

---
+++ src/api/account/index.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+// 계좌리스트
+export const fetchAccountData = () => {
+  return axios.get(`${baseUrl}/api/my/account?type=a_`);
+};
+
+// 연락처
+export const fetchContactData = () => {
+  return axios.get(`${baseUrl}/api/account/contact?type=a`);
+};
+
+// 최근 연락처
+export const fetchRecentData = () => {
+  return axios.get(`${baseUrl}/api/account/recent?type=a_`);
+};
+
+// 은행 데이터
+export const fetchBankData = () => {
+  return axios.get(`${baseUrl}/api/account/bank?type=a_`);
+};
+
+export const updateRemit = (data) => {
+  return axios.post(`${baseUrl}/api/my/account`, data)
+}
+
+
--- src/components/bankbook.tsx
+++ src/components/bankbook.tsx
@@ -2,17 +2,18 @@
 import styles from "./bankbook.module.css"
 import AccountList from "@/components/accountlist";
 import { useQuery } from "@tanstack/react-query";
-import { fetchAccountData } from "@/app/api/account";
+import { fetchAccountData } from "@/api/account";
+import {useEffect} from "react";
 
 const BankBook = () => {
   const { data: accountlist } = useQuery({
-    queryKey: ["banklist"],
+    queryKey: ["accountlist"],
     queryFn: () => fetchAccountData()
   });
 
   return (
       <ul className={styles.bankList}>
-        {accountlist?.map((item, idx) => (
+        {accountlist?.data.map((item, idx) => (
             <AccountList item={item} key={idx}  />
         ))}
       </ul>
